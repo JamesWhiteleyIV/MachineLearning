0e631cd48d57d9990858e19e2197f6435110adb8
James Whiteley <james@brud.fyi>
2021-01-13 08:14:24 -0800

    WIP learning

--- neural-networks-and-deep-learning/src/network.py
+++ neural-networks-and-deep-learning/src/network.py
@@ -9,11 +9,7 @@ simple, easily readable, and easily modifiable.  It is not optimized,
 and omits many desirable features.
 """
 
-#### Libraries
-# Standard library
 import random
-
-# Third-party libraries
 import numpy as np
 
 class Network(object):
@@ -27,24 +23,63 @@ class Network(object):
         won't set any biases for those neurons, since biases are only
         ever used in computing the outputs from later layers.
 
-        :param list size: first index is number of input layer neurons.
+        :param list size: 
+            first index is number of input layer neurons.
             last index in number of output layer neurons.
             each index in between first and last is a hidden layer of neurons.
 
         Example:
-            [2, 3, 1]  # 3-layer network, 2 input, 3 hidden, 1 output
+            [2, 3, 1]     # 3-layer network, 2 input, 3 hidden, 1 output
             [2, 3, 4, 1]  # 4-layer network, 2 input, 3 hidden, 4 hidden, 1 output
         """
-        self.num_layers = len(sizes)
-        self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.num_layers = len(sizes)  # e.g. 3
+        self.sizes = sizes            # e.g. [2, 3, 1]
+
+        """
+        # each neuron in each layer which is not input layer gets a bias assigned to it
+        # e.g. for [2, 3, 1] it will only create random biases for [3, 1]
+        [
+            [
+                [ 0.11990344]
+                [-1.38834335]
+                [-0.50051942]
+            ],
+            [
+                [-0.06048575]
+            ] 
+        ]
+        """
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]] 
+
+        """
+        # weights are the lines between each neuron
+        # e.g. weights[1] is a numpy matrix storing the weights connecting the second and third layers
+        # each input layer neuron goes to each second layer neuron  2 * 3 = 6 total weights
+        # each second layer neuron goes to each output layer neuron 3 * 1 = 3 total weights
+        [
+            [
+                [-1.58724458  1.39180206]
+                [-0.81362494 -1.50619335]
+                [-1.43461051 -3.25606635]
+            ],
+            [
+                [-0.55330222  0.43328301  0.26116272]
+            ]
+        ]
+        """
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+        # NOTE: this produces pairwise tuples
+        # x, y in zip(sizes[:-1], sizes[1:])
+        # e.g. [(2, 3), (3, 1)] for sizes=[2, 3, 1]
+        # so that we can create a matrices between each (see above)
+
 
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
+
 
     def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         """Train the neural network using mini-batch stochastic gradient descent.  
@@ -58,18 +93,22 @@ class Network(object):
             but slows things down substantially.
         """
         len_training_data = len(training_data)
+        if test_data is not None:
+            len_test_data = len(test_data)
 
         for j in xrange(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, len_training_data, mini_batch_size)]
 
+            # for each mini_batch, apply a single step of gradient descent
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
 
             if test_data is not None:
-                print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), len(test_data))
+                print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), len_test_data)
             else:
                 print "Epoch {0} complete".format(j)
+
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
@@ -86,6 +125,7 @@ class Network(object):
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
+
 
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
@@ -122,6 +162,7 @@ class Network(object):
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
+
     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
@@ -131,15 +172,18 @@ class Network(object):
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
+
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
         return (output_activations-y)
 
+
 #### Miscellaneous functions
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
+
 
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
